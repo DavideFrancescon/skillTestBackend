9c09ad26a6db49c6727adfbcdec00566f388bbcd
davfrance <dadefrancescon@gmail.com>
2022-08-09 11:55:52 +0200

    fix: auto wrote line

--- src/database/models/User.py
+++ src/database/models/User.py
@@ -1,5 +1,4 @@
 
-from msilib.schema import PublishComponent
 from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, create_engine
 
 from sqlalchemy.ext.declarative import declarative_base
